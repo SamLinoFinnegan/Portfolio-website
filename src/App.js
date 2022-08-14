485d2604f7219201c58c56a54ee68bf7ca10cd0e
Sam Finnegan <samuel.a.lino.sl@gmail.com>
2022-08-14 11:13:53 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./components/About/About";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from "./components/Footer/Footer";
 
 
@@ -14,13 +14,13 @@ function App() {
     <Router>
       <div>
         <NavBar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/home" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/skills" component={Skills} />
-          <Route path="/projects" component={Projects} />
-        </Switch>
+        <Routes>
+          <Route path="/"  component={<Home/>} />
+          <Route path="/home" component={<Home/>} />
+          <Route path="/about" component={<About/>} />
+          <Route path="/skills" component={<Skills/>} />
+          <Route path="/projects" component={<Projects/>} />
+        </Routes>
         <Footer />
       </div>
     </Router>
