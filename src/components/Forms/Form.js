a74b9195cfa948ec24d3ad13f017d54e02d67266
Sam Finnegan <samuel.a.lino.sl@gmail.com>
2022-10-11 14:55:53 +0100

    Added requierd filter to form

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -1,41 +1,28 @@
 import React from "react";
 import Style from './Form.module.css';
 import plane from './../../img/send_icon.png'
-import { useForm } from "react-hook-form";
 
 export default function Form() {
-  const { register, handleSubmit,  errors } = useForm();
-  const onSubmit = data => console.log(data);
     return (
       <form 
         method='post' 
         name='contactForm' 
-        className={Style.contactForm}
-        onSubmit={handleSubmit(onSubmit)}
-        >
+        className={Style.contactForm}>
         <div className={Style.inputNameAndEmail}>
         <input type="hidden" name="form-name" value="contactForm" />
         <input 
           type='text' 
-          name='name'
-          ref={register({ required: true })}
-          placeholder='Enter your name' 
-          />
-          {errors.requiredField && <span>This field is required</span>}
+          name='name' 
+          placeholder='Enter your name' />
+  
         <input 
           type='email' 
-          name='email'
-          ref={register({ required: true })} 
+          name='email' 
           placeholder='Enter your email' />
-          {errors.requiredField && <span>This field is required</span>}
         </div>
         <textarea 
           name='message' 
-          placeholder='Messaage'
-          ref={register({ required: true })}
-          >
-          {errors.requiredField && <span>This field is required</span>}
-          </textarea>
+          placeholder='Messaage'></textarea>
   
         <button type='submit'>
             <div><img src={plane} alt="send icon"></img></div>
