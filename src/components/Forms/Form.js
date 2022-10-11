84aec42078784157639f540fedff74d68419d4ee
Sam Finnegan <samuel.a.lino.sl@gmail.com>
2022-10-11 14:52:59 +0100

    Added requierd filter to form

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -4,7 +4,7 @@ import plane from './../../img/send_icon.png'
 import { useForm } from "react-hook-form";
 
 export default function Form() {
-  const { register, handleSubmit, watch, errors } = useForm();
+  const { register, handleSubmit,  errors } = useForm();
   const onSubmit = data => console.log(data);
     return (
       <form 
