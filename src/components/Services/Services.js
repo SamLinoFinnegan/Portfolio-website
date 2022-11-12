49450545e85bea7fdb0085dedc764021bca8b147
Sam Finnegan <samuel.a.lino.sl@gmail.com>
2022-11-12 13:47:12 +0000

    starting on services

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -7,7 +7,7 @@ const Services = () =>{
     return(
         <div className={Style.services}>
             <div className={Style.container}>
-                <img src={construction} alt="page_under_construction"></img>
+                <img src={construction} alt="page_under_construction" style={{width:600}}></img>
                 <h1>Page under construction</h1>
             </div>
             
