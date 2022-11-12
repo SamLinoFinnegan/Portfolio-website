8ac780b958f175682d3ea5ca2e0849265eea29c2
Sam Finnegan <samuel.a.lino.sl@gmail.com>
2022-11-12 13:33:56 +0000

    starting on services

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar/NavBar';
 import About from "./components/About/About";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
+import Services from "./components/Services/Services"
 import Skills from "./components/Skills/Skills";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from "./components/Footer/Footer";
